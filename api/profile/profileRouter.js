3102d669ac1258e8d889a52966ae10125daef658
Christina Yun <poncheeyun@yahoo.com>
2021-12-09 15:54:06 -0800

    Got rid of a console.log in the program

--- api/profile/profileRouter.js
+++ api/profile/profileRouter.js
@@ -67,7 +67,6 @@ router.get('/', function (req, res) {
       res.status(200).json(profiles);
     })
     .catch((err) => {
-      console.log(err);
       res.status(500).json({ message: err.message });
     });
 });
