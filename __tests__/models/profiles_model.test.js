d503211527ceb848bf3242c91e8ada3423770b58
Christina Yun <poncheeyun@yahoo.com>
2021-12-09 11:49:37 -0800

    Finished writing tests for the remove() function in profile model.

--- __tests__/models/profiles_model.test.js
+++ __tests__/models/profiles_model.test.js
@@ -61,17 +61,50 @@ describe('create(profile)', () => {
     expect(res[0]).toMatchObject(newProfile);
   });
 
-  it.todo('returns 9 profiles');
-  it.todo('returns the correct data shape');
+  it('returns 9 profiles', async () => {
+    const getAll = await db('profiles');
+
+    expect(getAll).toHaveLength(9);
+    expect(getAll[8]).toMatchObject(newProfile);
+  });
+  it('returns the correct data shape', () => {
+    expect(res).toMatchSnapshot();
+  });
 });
 
 describe('update(id, profile)', () => {
-  it.todo('placeholder');
-  it.todo('returns the correct data shape');
+  let res;
+  const oldProfile = {
+    id: 'bnsdj2',
+    email: 'Rei@gmail.com',
+    name: 'Rei Hino',
+    avatarUrl: 'https://tinyurl.com/4pf7d3v6',
+  };
+
+  const updatedProfile = {
+    id: 'bnsdj2',
+    email: 'Minako@gmail.com',
+    name: 'Minako Aino',
+    avatarUrl: 'https://tinyurl.com/4pf7d3v6',
+  };
+  beforeEach(async () => {
+    res = await Profiles.update(oldProfile.id, updatedProfile);
+  });
+  it('finds and returns correct profile with updated information', async () => {
+    const findById = await db('profiles')
+      .where({ id: 'bnsdj2' })
+      .select('*')
+      .first();
+    expect(res[0].name).toBe('Minako Aino');
+    expect(findById).toMatchObject(updatedProfile);
+  });
+  it('returns the correct data shape', () => {
+    expect(res).toMatchSnapshot();
+  });
 });
 
 describe('remove()', () => {
-  it.todo('placeholder');
+  it.todo('finds and removes the correct profile');
   it.todo('returns the correct data shape');
 });
 
